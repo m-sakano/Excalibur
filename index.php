61cbc5e5b6f3b34fadab8b23878b2a6e16fd5955
m-sakano@bfts.co.jp <m-sakano@bfts.co.jp>
2015-03-07 05:12:24 +0900

    update index

--- index.php
+++ index.php
@@ -45,16 +45,25 @@ session_start();
 
 <div class="container">
 	<div class="row">
-		<div class="col-sm-3" style="background:white;">
-		    <p> </p>
-		</div>
-		<div class="col-sm-6" style="background:white;">
+		<div class="col-sm-9" style="background:white;">
 			<div class="panel panel-info">
 				<div class="panel-heading">お知らせ</div>
   				<div class="panel-body">
     				Excaliburは現在開発中です。
   				</div>
 			</div>
+			
+			<!-- MyDeck List Selector DropDown button -->
+            <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    マイデッキ <span class="caret"></span>
+                </button>
+                <?php showDeckList(); ?>
+            </div>
+            <!-- MyDeck Table View -->
+            <?php showDeck(); ?>
+            
+            <hr>
 		    <h1>ようこそ</h1>
 		    <p>Excaliburは『乖離性ミリオンアーサー』のデッキ構築を支援します。<br>
 		    	デッキはアーサーごとに10個までセーブできます。<br>
@@ -84,3 +93,103 @@ session_start();
 <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
+
+<?php
+/**
+ * デッキリストを表示する 
+ * 
+ */
+function showDeckList() {
+    echo '<ul class="dropdown-menu" role="menu">';
+    for ($i=1; $i<=10; $i++) {
+        if($_SESSION['deck'][$i]) {
+            $deckName = $_SESSION['deck'][$i];
+        } else {
+            $deckName = "マイデッキ $i";
+        }
+        echo '<li><a href="./?deck='.$i.'">'.$deckName.'</a></li>';
+    }
+    echo '</ul>';
+}
+
+/**
+ * デッキをテーブル表示する
+ * 
+ */
+function showDeck() {
+    echo '<table class="table table-striped table-bordered">';
+    echo '<thead>';
+    echo '<tr>';
+    echo '<th>番号</th>';
+    echo '<th>称号</th>';
+    echo '<th>名前</th>';
+    echo '<th>レア</th>';
+    echo '<th>コスト</th>';
+    echo '<th>アーサー</th>';
+    echo '<th>タイプ</th>';
+    echo '<th>属性</th>';
+    echo '<th>ＨＰ</th>';
+    echo '<th>物理</th>';
+    echo '<th>魔法</th>';
+    echo '<th>回復</th>';
+    echo '<th>通常スキル</th>';
+    echo '<th>覚醒スキル</th>';
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+
+    if($_GET['deck']){
+        $deckNum = h($_GET['deck']);
+    } else {
+        $deckNum = '1';
+    }
+    for($cardNum=1; $cardNum<=10; $cardNum++) {
+        if($_SESSION['deck'][$deckNum][$cardNum]) {
+            $title = h($_SESSION['deck'][$deckNum][$cardNum]['title']);
+            $name  = h($_SESSION['deck'][$deckNum][$cardNum]['name']);
+            $sql = "select * from cards where Title='$title' and Name='$name' ";
+            $dbh = connectDb();
+            $stmt = $dbh->query("SET NAMES utf8;");
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute();
+        
+            while($record = $stmt->fetch()) {
+                $line = '';
+                $line .= '<tr>';
+                $line .= '<td>'.$cardNum.'</td>';
+                $line .= '<td>'.$record['Title'].'</td>';
+                $line .= '<td>'.$record['Name'].'</td>';
+                $line .= '<td>'.$record['Rare'].'</td>';
+                $line .= '<td>'.$record['Cost'].'</td>';
+                $line .= '<td>'.$record['Arthur'].'</td>';
+                $line .= '<td>'.$record['Type'].'</td>';
+                $line .= '<td>'.$record['Attribute'].'</td>';
+                $line .= '<td>'.$record['BonusHP'].'</td>';
+                $line .= '<td>'.$record['BonusPhysical'].'</td>';
+                $line .= '<td>'.$record['BonusMagic'].'</td>';
+                $line .= '<td>'.$record['BonusHeal'].'</td>';
+                $line .= '<td>'.$record['SkillNormal'].'</td>';
+                $line .= '<td>'.$record['SkillSpecial'].'</td>';
+                $line .= '</tr>';
+                echo $line;
+            }
+            $stmt->closeCursor();
+        } else {
+            $line = '';
+            $line .= '<tr>';
+            $line .= "<td>$cardNum</td>";
+            for($i=0;$i<13;$i++) {
+                $line .= '<td></td>';
+            }
+            $line .= '</tr>';
+            echo $line;
+        }
+    }
+    echo '</tbody>';
+    echo '</table>';
+}
+
+
+
+
+?>
